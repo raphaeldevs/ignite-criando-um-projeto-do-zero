e8856fd455a44b44b81309a9b96aa910375d8ad0
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-27 17:43:38 -0300

    create post page

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,11 +1,15 @@
-import { GetStaticPaths, GetStaticProps } from 'next';
+// import { GetStaticPaths, GetStaticProps } from 'next';
 
-import { getPrismicClient } from '../../services/prismic';
+// import { getPrismicClient } from '../../services/prismic';
+
+import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+
+import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
-interface Post {
+/* interface Post {
   first_publication_date: string | null;
   data: {
     title: string;
@@ -24,22 +28,87 @@ interface Post {
 
 interface PostProps {
   post: Post;
+} */
+
+export default function Post(): JSX.Element {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.container}>
+        <img src="/banner.png" alt="phone" />
+
+        <article className={commonStyles.container}>
+          <header>
+            <h1>Criando um app CRA do zero</h1>
+            <div className={commonStyles.postInfo}>
+              <time>
+                <FiCalendar />
+                15 Mar 2021
+              </time>
+
+              <span>
+                <FiUser />
+                Raphael Corrêa
+              </span>
+
+              <span>
+                <FiClock /> 4 min
+              </span>
+            </div>
+          </header>
+
+          <main>
+            <h2>Proin et varius</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <p>
+              Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
+              tellus. <strong>Nam facilisis sodales</strong> felis, pharetra
+              pharetra lectus auctor sed.
+            </p>
+
+            <p>
+              Ut venenatis mauris vel libero pretium, et pretium ligula
+              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
+              erat. Nam venenatis.
+            </p>
+
+            <ul>
+              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit</li>
+            </ul>
+
+            <h2>Proin et varius</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+            <p>
+              Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
+              tellus. Nam facilisis sodales felis, pharetra pharetra lectus
+              auctor sed.
+            </p>
+
+            <p>
+              Ut venenatis mauris vel libero pretium, et pretium ligula
+              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
+              erat. Nam venenatis.
+            </p>
+          </main>
+        </article>
+      </div>
+    </>
+  );
 }
 
-// export default function Post() {
-//   // TODO
-// }
+/* export const getStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query(TODO);
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+  // TODO
+};
 
-//   // TODO
-// };
+export const getStaticProps = async context => {
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID(TODO);
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
-
-//   // TODO
-// };
+  // TODO
+}; */
