0c6af2d62929362b8db83e2c7281620b67a0acf7
Raphael Corrêa <raphaeldeoliveiracorrea@gmail.com>
2021-03-28 19:44:59 -0300

    feat: get post by slug and handle  static paths

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,15 +1,25 @@
-// import { GetStaticPaths, GetStaticProps } from 'next';
+import { useMemo } from 'react';
 
-// import { getPrismicClient } from '../../services/prismic';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
+
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 import { FiCalendar, FiClock, FiUser } from 'react-icons/fi';
+
+import Prismic from '@prismicio/client';
+
+import { RichText } from 'prismic-dom';
+
+import { getPrismicClient } from '../../services/prismic';
 
 import Header from '../../components/Header';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
 
-/* interface Post {
+interface Post {
   first_publication_date: string | null;
   data: {
     title: string;
@@ -28,70 +38,69 @@ import styles from './post.module.scss';
 
 interface PostProps {
   post: Post;
-} */
+}
 
-export default function Post(): JSX.Element {
+export default function Post({ post }: PostProps): JSX.Element {
+  const router = useRouter();
+
+  const readTime = useMemo(() => {
+    const HUMAN_READ_WORDS_PER_MINUTE = 200;
+
+    const words = post?.data?.content?.reduce((contentWords, content) => {
+      contentWords.push(...content.heading.split(' '));
+
+      const sanitizedContent = RichText.asText(content.body)
+        .replace(/[^\w|\s]/g, '')
+        .split(' ');
+
+      contentWords.push(...sanitizedContent);
+
+      return contentWords;
+    }, []);
+
+    return Math.ceil(words?.length / HUMAN_READ_WORDS_PER_MINUTE);
+  }, [post]);
+
   return (
     <>
       <Header />
 
       <div className={styles.container}>
-        <img src="/banner.png" alt="phone" />
+        <img src={post?.data.banner.url} alt={post?.data.author} />
 
         <article className={commonStyles.container}>
           <header>
-            <h1>Criando um app CRA do zero</h1>
+            <h1>{router.isFallback ? 'Carregando...' : post?.data.title}</h1>
             <div className={commonStyles.postInfo}>
               <time>
                 <FiCalendar />
-                15 Mar 2021
+                {format(new Date(post?.first_publication_date), 'dd MMM yyyy', {
+                  locale: ptBR,
+                }) ?? 'Data de publicação'}
               </time>
 
               <span>
                 <FiUser />
-                Raphael Corrêa
+                {post?.data.author ?? 'Autor'}
               </span>
 
               <span>
-                <FiClock /> 4 min
+                <FiClock /> {readTime ? `${readTime} min` : 'Tempo de leitura'}
               </span>
             </div>
           </header>
 
           <main>
-            <h2>Proin et varius</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-            <p>
-              Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
-              tellus. <strong>Nam facilisis sodales</strong> felis, pharetra
-              pharetra lectus auctor sed.
-            </p>
-
-            <p>
-              Ut venenatis mauris vel libero pretium, et pretium ligula
-              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
-              erat. Nam venenatis.
-            </p>
-
-            <ul>
-              <li>Lorem ipsum dolor sit amet consectetur adipisicing elit</li>
-            </ul>
-
-            <h2>Proin et varius</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-
-            <p>
-              Nullam dolor sapien, vulputate eu diam at, condimentum hendrerit
-              tellus. Nam facilisis sodales felis, pharetra pharetra lectus
-              auctor sed.
-            </p>
-
-            <p>
-              Ut venenatis mauris vel libero pretium, et pretium ligula
-              faucibus. Morbi nibh felis, elementum a posuere et, vulputate et
-              erat. Nam venenatis.
-            </p>
+            {post?.data?.content?.map(groupContent => (
+              <div key={groupContent.heading}>
+                <h2>{groupContent.heading}</h2>
+                <div
+                  dangerouslySetInnerHTML={{
+                    __html: RichText.asHtml(groupContent.body),
+                  }}
+                />
+              </div>
+            ))}
           </main>
         </article>
       </div>
@@ -99,16 +108,50 @@ export default function Post(): JSX.Element {
   );
 }
 
-/* export const getStaticPaths = async () => {
+export const getStaticPaths: GetStaticPaths = async () => {
   const prismic = getPrismicClient();
-  const posts = await prismic.query(TODO);
+  const posts = await prismic.query(
+    [Prismic.predicates.at('document.type', 'posts')],
+    {
+      pageSize: 2,
+    }
+  );
 
-  // TODO
+  const paths = posts.results.map(post => ({
+    params: {
+      slug: post.uid,
+    },
+  }));
+
+  return {
+    fallback: true,
+    paths,
+  };
 };
 
-export const getStaticProps = async context => {
-  const prismic = getPrismicClient();
-  const response = await prismic.getByUID(TODO);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { slug } = params;
 
-  // TODO
-}; */
+  const prismic = getPrismicClient();
+  const response = await prismic.getByUID('posts', String(slug), {});
+
+  const post = {
+    uid: response.uid,
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      subtitle: response.data.subtitle,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content,
+    },
+  };
+
+  return {
+    props: {
+      post,
+    },
+  };
+};
